a0ce9fb4bb29dee926b1a3372e784149be95d5ae
mariamm015 <mariemmasoud465@gmail.com>
2025-06-23 01:50:10 +0300

    Add files via upload

---
+++ my.js
@@ -0,0 +1,36 @@
+let length = 5;
+let width = 3;
+let areaRectangle = length * width;
+
+console.log("The area of the rectangle is: " + areaRectangle);
+    
+
+
+let radius = 4;
+let areaCircle = 3.14 * radius * radius;
+
+console.log("The area of the circle is: " + areaCircle);
+
+
+
+let secretNumber = 5;
+let guess = 3;
+
+if (guess === secretNumber) {
+    console.log("Congratulations! You guessed the correct number.");
+} else {
+    console.log("Wrong guess. Try again.");
+}
+
+
+
+
+let Number = 7;
+let yourguess = 3;
+
+while (yourguess !== Number) {
+    console.log("Wrong guess. Try again.");
+    yourguess = 7;
+}
+
+console.log("Congratulations! You guessed the correct number.");
